f53cb91735ad36c63d3651c6975e439826992e01
armandolopz <armandolopez0611@gmail.com>
2026-02-02 19:20:00 -0400

    feat: Configure TypeORM with SQLite for User and Role entity management and update package dependencies.

--- sana-ia-backend/src/app.module.ts
+++ sana-ia-backend/src/app.module.ts
@@ -1,11 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
 import { RolesModule } from './roles/roles.module';
 
 @Module({
-  imports: [UsersModule, UsersModule, RolesModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db.sqlite',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    UsersModule,
+    RolesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
